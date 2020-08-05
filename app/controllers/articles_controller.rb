28f6aa3aaf9fe919559e1d1234bafd1a2107f2f8
Tomoakiasano <tomoharu114@gmail.com>
2020-08-06 00:24:07 +0900

    残り修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
       @article = Article.new(article_params)
       if @article.valid?
         @article.save
-        redirect_to(article_path(@article))       
+        redirect_to(article_path(@article))
       else
         render 'form'
       end
--- spec/controllers/errors_controller_spec.rb
+++ spec/controllers/errors_controller_spec.rb
@@ -1,20 +1,37 @@
 require 'rails_helper'
 
-# RSpec.describe ErrorsController, type: :controller do
 describe ErrorsController, type: :controller do
   describe '#render_404' do
+    render_views
+
     controller do
       def index
         render_404
       end
     end
+    
     let(:error) { ActiveRecord::RecordNotFound }
     it '404ページが表示されること' do
       get :index
-      expect(response.status).to have_http_status(:not_found)
+      expect(response.status).to eq 404
       expect(response).to render_template('errors/error_404')
     end
   end
-  
 
+  describe '#render_505' do
+    render_views
+
+    controller do
+      def index
+        render_500
+      end
+    end
+
+    let(:error) { StandardError }
+    it '500ページが表示されること' do
+      get :index
+      expect(response.status).to eq 500
+      expect(response).to render_template('errors/error_500')
+    end
+  end
 end
